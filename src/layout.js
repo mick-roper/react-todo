3de829cc2dd79da957f407514c3a8cf8fc9c205f
Mick Roper <mick.roper@peoplesholdings.co.uk>
2018-01-19 11:23:13 +0000

    started assembling components

--- src/layout.js
+++ src/layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
+import TodoList from './components/todo-list';
+
 export default class Layout extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hello, World!</h1>
+                <TodoList />
             </div>
         );
     }
